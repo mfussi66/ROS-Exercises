caca27bc2335d0e6e7dbc26d75395e1f5eb7b367
Mattia <mattia.fussi@protonmail.com>
2019-09-08 16:45:37 +0200

    Refined launcher

--- README.md
+++ README.md
@@ -2,35 +2,37 @@
 The following is a collection of small exercises written in C++ within the ROS middleware for a course in Distributed Robotic Systems.
 
 ## Building
-The catkin software was used to build the programs. Copy the exercises in your catkin source directory and type
+The catkin software was used to build the programs. Copy the exercises in your catkin source directory and run
 ```
 catkin_make 
 ```
 ## Running
-To run the exercises, first run ```roscore``` then follow the instructions:
+To run the exercises, follow the instructions:
 
 ### Exercise 1
-In a separate terminal, run:
+Exercise 1 uses the RViz package to visualize the robot pose.
+In a terminal, run:
 ``` 
 roslaunch exercise1 exercise1.launch
 ```
 ### Exercise 2
-In a separate terminal, run:
+Exercise 2 needs parameters for linear and angular speed saturations: 
+In a terminal, run:
 ```
 roslaunch exercise2 exercise2.launch
 ```
 ### Exercise 3
-In a separate terminal, run:
+In a terminal, run:
 ``` 
 roslaunch exercise3 exercise3.launch
 ```
 ### Exercise 4
-In a separate terminal, run:
+In a terminal, run:
 ```
 roslaunch exercise4 exercise4.launch
 ```
 ### Exercise 5
-In a separate terminal, run:
+In a terminal, run:
 ```
 roslaunch exercise5 exercise5.launch
 ```
--- exercise2/src/ex2_ctrl.cpp
+++ exercise2/src/ex2_ctrl.cpp
@@ -1,22 +1,20 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 #include "exercise2/control.h"
-#include <iostream>
-
-ros::Publisher pub;
 
 float v;
 float w;
 
-void commandCallback(const exercise2::control::ConstPtr& msg)
-{
-    ROS_INFO("Callback is working!");
+void cmd_callback(const exercise2::control::ConstPtr& msg){
+  /* Callback to save velocities coming from /control custom topic */
+  
     v = msg->lin_vel;
     w = msg->ang_vel;
 }
 
-float checkLimits(float var, float var_min, float var_max)
-{
+float check_limits(float var, float var_min, float var_max){
+  /* check velocity limits to apply saturations*/
+  
     if (var > var_max)
     {
         ROS_INFO("value higher than saturation - Capping.");
@@ -31,44 +29,41 @@ float checkLimits(float var, float var_min, float var_max)
     return var;
 }
 
-int main(int argc, char **argv)
-{
-    ros::init(argc, argv, "exercise2");
-    ros::NodeHandle nh("~");
+int main(int argc, char **argv){
+  
+  ros::init(argc, argv, "exercise2");
+  ros::NodeHandle nh("~");      
+  ros::Rate rate(100);
 
-    double v_min = -1.0;
-    double v_max = 1.0;
-    double w_min = -1.0;
-    double w_max = 1.0;
-    
-    nh.getParam("vel_lin_min", v_min);
-    nh.getParam("vel_lin_max", v_max);
-    nh.getParam("vel_ang_min", w_min);
-    nh.getParam("vel_ang_max", w_max);
-    
-    ros::Rate rate(100);
-    
-    ros::Subscriber sub = nh.subscribe("/control", 10, &commandCallback);
-    
-    ROS_INFO("Subscribing to topic: /control");
-    
-    pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
-    sleep(1);
-    
-    while (ros::ok())
-    {
-        geometry_msgs::Twist cmd;
-        cmd.linear.x = checkLimits(v, v_min, v_max);
-        cmd.angular.z = checkLimits(w, w_min, w_max);
-        pub.publish(cmd);
-        
-        //ROS_INFO("Sending v: %f, w: %f", v, w);
-        
-        ros::spinOnce();
-        rate.sleep();
-    }
+  double v_min = -1.0;
+  double v_max = 1.0;
+  double w_min = -1.0;
+  double w_max = 1.0;
 
-	return 0;
+  nh.getParam("vel_lin_min", v_min);
+  nh.getParam("vel_lin_max", v_max);
+  nh.getParam("vel_ang_min", w_min);
+  nh.getParam("vel_ang_max", w_max);
 
+  ros::Subscriber sub = nh.subscribe("/control", 10, &cmd_callback);
+  ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
+
+  ROS_INFO("Subscribing to topic: /control");
+
+  sleep(1);
+
+  while (ros::ok())
+  {
+      geometry_msgs::Twist cmd;
+      cmd.linear.x = check_limits(v, v_min, v_max);
+      cmd.angular.z = check_limits(w, w_min, w_max);
+      pub.publish(cmd);
+      
+      //ROS_INFO("Sending v: %f, w: %f", v, w);
+      
+      ros::spinOnce();
+      rate.sleep();
+  }
+  return 0;
 }
 
